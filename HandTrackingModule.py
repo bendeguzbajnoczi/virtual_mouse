e4adb66e8b33076e217c222bc2654fcef7599569
bendeguzbajnoczi <53899928+bendeguzbajnoczi@users.noreply.github.com>
2022-11-09 16:45:03 +0100

    Update HandTrackingModule.py
    Change finger up detection based on distance from wrist

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -17,21 +17,23 @@ class handDetector():
                                         self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
+        self.baseIds = [1, 5, 9, 13, 17]
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
         # print(results.multi_hand_landmarks)
 
+        retigm = img
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
+                    self.mpDraw.draw_landmarks(retigm, handLms,
                                                self.mpHands.HAND_CONNECTIONS)
 
-        return img
+        return retigm
 
-    def findPosition(self, img, handNo=0, draw=True):
+    def findPosition(self, img, handNo=0, draw=False):
         xList = []
         yList = []
         bbox = []
@@ -62,33 +64,31 @@ class handDetector():
     def fingersUp(self):
         fingers = []
         # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
+        tipToRingBAseDistance = self.findDistance(self.tipIds[0], 0)
+        wristToRingBaseDistance = self.findDistance(self.baseIds[3], 0)
+
+        if wristToRingBaseDistance > tipToRingBAseDistance:
             fingers.append(0)
+        else:
+            fingers.append(1)
 
         # Fingers
         for id in range(1, 5):
 
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 1][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+            tipToWristDistance = self.findDistance(self.tipIds[id], 0)
+            firstJoinToWristDistance = self.findDistance(self.tipIds[id]-1, 0)
 
-        # totalFingers = fingers.count(1)
+            if tipToWristDistance < firstJoinToWristDistance:
+                fingers.append(0)
+            else:
+                fingers.append(1)
 
         return fingers
 
-    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+    def findDistance(self, p1, p2):
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
         length = math.hypot(x2 - x1, y2 - y1)
 
-        return length, img, [x1, y1, x2, y2, cx, cy]
+        return length
