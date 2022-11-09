b165ca35ef04d89e4c38945744e3008ee7dc8a57
bendeguzbajnoczi <53899928+bendeguzbajnoczi@users.noreply.github.com>
2022-11-09 16:43:49 +0100

    Update main.py
    Fix bug where window update stops if no hand is detected
    Make fingerDown detection based on distance from wrist instead of y coordinate difference
    Follow middle of hand instead of index finger, so detecting on the edges is more stable
    Change move cursor to open palm
    Change rigthclick to index finger down
    Chande rigth click to middle finger down
    Change double click to index and middle finger down
    Change mouse.click() to mouse.press/release so drag is possible
    Create scroll (closed fist moving up and down)
    Create hand gesture to stop program: middle finger up XD

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import tkinter as tk
 ##########################
 wCam, hCam = 640, 480
 frameR = 100  # Frame Reduction
-smoothening = 7
+smoothening = 6
 #########################
 
-pTime = 0
-plocX, plocY = 0, 0
 clocX, clocY = 0, 0
 
 cap = cv2.VideoCapture(0)
@@ -23,95 +21,123 @@ root = tk.Tk()
 scrW = root.winfo_screenwidth()
 scrH = root.winfo_screenheight()
 
-clicked = False
-dclicked = False
-rclicked = False
+leftClicked = False
+leftPress = False
+doubleLeftClicked = False
+rightClicked = False
+wheel = False
 
-while True:
-    #1. Preprocess
-    success, img_o = cap.read()
-    # 1.1 Contrast stetching with CLACHE
-    # img_o = cv2.normalize(img_o, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    # img_hsv = cv2.cvtColor(img_o, cv2.COLOR_BGR2HSV)
-    # h, s, v = img_hsv[:, :, 0], img_hsv[:, :, 1], img_hsv[:, :, 2]
-    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-    # v = clahe.apply(v)
-    # hsv_img = np.dstack((h, s, v))
-    # bgr_stretched = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2RGB)
+handPositionX = 0
+handPositionY = 0
+
+mousePositionX = 0
+mousePositionY = 0
+
+scrollStartX = 0
+scrollStartY = 0
+
+delta = 0
+run = True
+turnOffCounter = 5
+
+while run:
+    # 1. Preprocess
+    success, img = cap.read()
 
     # 1.2 Image sharpening
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-    image_sharp = cv2.filter2D(src=img_o, ddepth=-1, kernel=kernel)
+    img = cv2.filter2D(src=img, ddepth=-1, kernel=kernel)
 
     # 2. Find hand Landmarks
-    img = detector.findHands(image_sharp)
+    img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
+
     if lmList != None:
         # 3. Get the tip of the index and middle fingers
         if len(lmList) != 0:
-            x1, y1 = lmList[8][1:]
-            x2, y2 = lmList[12][1:]
+            pointingFingerBaseX, pointingFingerBaseY = lmList[5][1:]
+            pinkyFingerBaseX, pinkyFingerBaseY = lmList[17][1:]
+
+            handPositionX = int((pointingFingerBaseX + pinkyFingerBaseX) / 2)
+            handPositionY = int((pointingFingerBaseY + pinkyFingerBaseY) / 2)
+
+            # 5. Convert Coordinates
+            mousePositionX = np.interp(handPositionX, (frameR, wCam - frameR), (0, scrW))
+            mousePositionY = np.interp(handPositionY, (frameR, hCam - frameR), (0, scrH))
 
         # 4. Check which fingers are up
         try:
             fingers = detector.fingersUp()
         except:
-            # flip = cv2.flip(img_o, 1)
-            # cv2.imshow("Image", flip)
-            continue
+                fingers = []
 
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-                      (255, 0, 255), 2)
-        # 4.1 Only Index Finger : Moving Mode
-        if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
-            clicked = False
-            dclicked = False
-            rclicked = False
-            # 5. Convert Coordinates
-            x3 = np.interp(x1, (frameR, wCam - frameR), (0, scrW))
-            y3 = np.interp(y1, (frameR, hCam - frameR), (0, scrH))
-            # 6. Smoothen Values
-            clocX = plocX + (x3 - plocX) / smoothening
-            clocY = plocY + (y3 - plocY) / smoothening
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+        if len(fingers) != 0:
+            # 4.1 (index middle ring and pinky up)) : Moving Mode
+            if fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
+                # 4.1.1 set clicks to false
+                doubleLeftClicked = False
+                rightClicked = False
+                wheel = False
 
-            # 7. Move Mouse
-            mouse.move(scrW - clocX, clocY)
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            delta = y3 - plocY
-            plocX, plocY = clocX, clocY
+                # 4.1.2 Smoothen Values
+                clocX = clocX + (mousePositionX - clocX) / smoothening
+                clocY = clocY + (mousePositionY - clocY) / smoothening
 
-        # 4.2 : Index and Middle fingers: Single Left Click
-        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0:
-            print("left click")
-            if not clicked:
-                clicked = True
-                mouse.click()
+                # 4.1.3 Move Mouse
+                mouse.move(scrW - clocX, clocY)
+                cv2.circle(img, (handPositionX, handPositionY), 10, (0, 0, 255), cv2.FILLED)
 
-        # 4.2 : Index, Middle, Ring and Little fingers: Double Left Click
-        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
-            print("dleft click")
-            if not dclicked:
-                dclicked = True
-                mouse.double_click()
+            # 4.2 : Index Left press
+            if fingers[1] == 0 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
+                if not leftPress:
+                    mouse.press('left')
+                    leftPress = True
+                    print("left press")
 
-        # 4.3 : Index and Little fingers: Single Right Click
-        if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 1:
-            print("right click")
-            if not rclicked:
-                rclicked = True
-                mouse.right_click()
+            if fingers[1] == 1:
+                if leftPress:
+                    mouse.release('left')
+                    leftPress = False
+                    print("left release")
 
-        # 4.4 Wheel Mode
-        # if fingers[1] == 0 and fingers[0] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
-        #     print("wheel")
-        #     if delta < 0:
-        #         mouse.wheel(delta=1)
-        #     if delta > 0:
-        #         mouse.wheel(delta=-1)
+            # 4.2 : Index, Middle, Ring and Little fingers: Double Left Click
+            if fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 1 and fingers[4] == 1:
+                if not doubleLeftClicked and not leftPress:
+                    mouse.double_click()
+                    doubleLeftClicked = True
+                    print("double left click")
+
+            # 4.3 : Middle finger down: Single Right Click
+            if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 1 and fingers[4] == 1:
+                if not rightClicked and not leftPress:
+                    mouse.right_click()
+                    rightClicked = True
+                    print("right click")
+
+            # 4.4 Wheel Mode
+            if fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
+                if not wheel and not leftPress:
+                    scrollStartX = mousePositionX
+                    scrollStartY = mousePositionY
+                    wheel = True
+                    print("wheel")
+                delta = scrollStartY-mousePositionY
+                if abs(delta) > 50:
+                    mouse.wheel(delta/200)
+
+            # 5 Stop
+            if fingers[1] == 0 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0:
+                turnOffCounter += 1
+                if turnOffCounter > 20:
+                    print("fuck")
+                    run = False
+            else:
+                turnOffCounter = 0
 
     # 5. Display
     flip = cv2.flip(img, 1)
-    cv2.imshow("Image", flip)
+    cv2.imshow("Detected", flip)
     cv2.waitKey(1)
